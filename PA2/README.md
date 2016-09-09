07a5a6fac57b8cf601120ec7cfe1040f22773c07
hannahmunoz <hannahmunoz92@gmail.com>
2016-09-09 11:34:08 -0700

    changed keyboard input to switch statements, got circle to turn counter clockwise. Need to make the keys do proper things now

---
+++ PA2/README.md
@@ -0,0 +1,26 @@
+# PA1: Spinning Cube
+
+# Dependencies, Building, and Running
+
+## Dependency Instructions
+For both of the operating systems to run this project installation of these three programs are required [GLEW](http://glew.sourceforge.net/), [GLM](http://glm.g-truc.net/0.9.7/index.html), and [SDL2](https://wiki.libsdl.org/Tutorials).
+
+This project uses OpenGL 3.3. Some computers, such as virtual machines in the ECC, can not run this version. In in order to run OpenGL 2.7 follow the instructions at [Using OpenGL 2.7](https://github.com/HPC-Vis/computer-graphics/wiki/Using-OpenGL-2.7)
+
+
+## Building and Running
+The project will work using either CMake or the make instructions
+
+### CMake Instructions
+cd build
+cmake ..
+make
+./Tutorial
+
+### Makefile Instructions 
+cd build
+cp ../makefile .
+make
+./Tutorial
+```
+
---
+++ PA2/src/object.cpp
@@ -0,0 +1,144 @@
+#include "object.h"
+
+Object::Object()
+{  
+  /*
+    # Blender File for a Cube
+    o Cube
+    v 1.000000 -1.000000 -1.000000
+    v 1.000000 -1.000000 1.000000
+    v -1.000000 -1.000000 1.000000
+    v -1.000000 -1.000000 -1.000000
+    v 1.000000 1.000000 -0.999999
+    v 0.999999 1.000000 1.000001
+    v -1.000000 1.000000 1.000000
+    v -1.000000 1.000000 -1.000000
+    s off
+    f 2 3 4
+    f 8 7 6
+    f 1 5 6
+    f 2 6 7
+    f 7 8 4
+    f 1 4 8
+    f 1 2 4
+    f 5 8 6
+    f 2 1 6
+    f 3 2 7
+    f 3 7 4
+    f 5 1 8
+  */
+
+  Vertices = {
+    {{1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{1.0f, -1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+    {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+    {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 1.0f}},
+    {{1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 0.0f}},
+    {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}},
+    {{-1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}},
+    {{-1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 1.0f}}
+  };
+
+  Indices = {
+    2, 3, 4,
+    8, 7, 6,
+    1, 5, 6,
+    2, 6, 7,
+    7, 8, 4,
+    1, 4, 8,
+    1, 2, 4,
+    5, 8, 6,
+    2, 1, 6,
+    3, 2, 7,
+    3, 7, 4,
+    5, 1, 8
+  };
+
+  // The index works at a 0th index
+  for(unsigned int i = 0; i < Indices.size(); i++)
+  {
+    Indices[i] = Indices[i] - 1;
+  }
+
+  angle = 0.0f;
+
+  glGenBuffers(1, &VB);
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
+
+  glGenBuffers(1, &IB);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+}
+
+Object::~Object()
+{
+  Vertices.clear();
+  Indices.clear();
+}
+
+void Object::Update(unsigned int dt,unsigned int pressedKey)
+{
+
+  // move the model in a circle
+  // The multiplication makes it go in a wider circle
+  switch (pressedKey){
+      // left
+      case 97: 
+         angle += dt * M_PI/5000;
+          std::cout << "left: " << pressedKey << std::endl;
+             model = glm::translate (glm::mat4(1.0f), glm::vec3(8*cos (angle), 0.0f , 8*sin (angle)));
+            model *= glm::rotate(glm::mat4(1.0f), (angle), glm::vec3(0.0, 1.0, 0.0));
+          break;
+     // right
+     // rotates square counter clockwise
+      case 100:
+        angle -= dt * M_PI/5000;
+        std::cout << "right: " << pressedKey << std::endl;
+        model = glm::translate (glm::mat4(1.0f), glm::vec3(8*cos (angle), 0.0f , 8*sin (angle)));
+        model *= glm::rotate(glm::mat4(1.0f), (angle), glm::vec3(0.0, 1.0, 0.0));
+
+        break;
+      case 119:
+          angle += dt * M_PI/5000;
+          std::cout << "up: " << pressedKey << std::endl;
+          break;
+      case 115:
+          angle += dt * M_PI/5000;
+          std::cout << "down: " << pressedKey << std::endl;
+          break;
+      case 32:
+          std::cout << "down: " << pressedKey << std::endl;
+            model = glm::translate (glm::mat4(1.0f), glm::vec3(8*cos (angle), 0.0f , 8*sin (angle)));
+            model = glm::rotate(model, (angle), glm::vec3(0.0, 1.0, 0.0));
+          break;
+      default:
+            angle += dt * M_PI/5000;
+            model = glm::translate (glm::mat4(1.0f), glm::vec3(8*cos (angle), 0.0f , 8*sin (angle)));
+            model *= glm::rotate(glm::mat4(1.0f), (angle), glm::vec3(0.0, 1.0, 0.0));
+          break;
+    }
+}
+
+glm::mat4 Object::GetModel()
+{
+  return model;
+}
+
+void Object::Render()
+{
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,color));
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+
+  glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+}
+
