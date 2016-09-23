cf4f5d9560a9f33fd9db7ae3ebeb7e2def96a6b3
hannahmunoz <hannahmunoz92@gmail.com>
2016-09-23 11:27:56 -0700

    model loader working, dragon acting as moon for stress test. No colors, don't know why

---
+++ PA4/include/object.h
@@ -0,0 +1,33 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include <string>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object(std::string file);
+    ~Object();
+    void Update(unsigned int dt, unsigned int pressedKey);
+    void Update(unsigned int dt, unsigned int pressedKey, glm::mat4 planet);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float rotateAngle;
+    float translateAngle;
+    glm::mediump_vec2 direction = {1,1};
+    
+};
+
+
+#endif /* OBJECT_H */
