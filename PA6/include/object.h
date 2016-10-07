78c55235d90ef233246ebf8f11a8b3c1964e514f
hannahmunoz <hannahmunoz92@gmail.com>
2016-10-06 17:07:18 -0700

    cmake working

--- PA6/include/object.h
+++ PA6/include/object.h
@@ -10,7 +10,7 @@
 #include <assimp/scene.h> //includes the aiScene object
 #include <assimp/postprocess.h> //includes the postprocessing variables for the importer
 #include <assimp/color4.h> //includes the aiColor4 object, which is used to handle the colors from the mesh objects 
-
+#include <Magick++.h>  // magick header
 class Object
 {
   public:
