5ec705a336284c38e1d1217bed1f98913c0236a3
hannahmunoz <hannahmunoz92@gmail.com>
2016-12-07 17:20:41 -0800

    hannahs stuff

--- PA11/src/UserTank.cpp
+++ PA11/src/UserTank.cpp
@@ -3,12 +3,14 @@
 #define MAXAMMO 5;
 
 UserTank::UserTank(){
-	user.base = new Object("tankbase.obj", 1000, btVector3(0, 0, 0), btVector3(20, -2.5, 5), .9, 0, 0, 5);
-	//adjust so that it lied on top of the base
-	user.head = new Object("turret.obj", 1000, btVector3(0, 0, 0), btVector3(20, 4.0995, 5), 1, 0, 0, 5);
-	user.placeholder = new Object("placeholder.obj");
-	SetOrientation();
 
+	user.base = new Object("tradeFederationTank.obj", 1000, btVector3(0, 0, 0), btVector3(20, 5, 5), .9, 0, .9, 5);
+
+	//adjust so that it lied on top of the base
+	//user.head = new Object("turret.obj", 1000, btVector3(0, 0, 0), btVector3(20, 4.0995, 5), 1, 0, 0, 5);
+	user.placeholder = new Object("placeholder.obj");
+	user.projectile = NULL;
+	SetOrientation();
 	//user.lives = MAXLIVES;
 	user.lives = 3;
 	user.compassPosition = 4; 
@@ -18,9 +20,10 @@ UserTank::UserTank(){
 	user.base->GetRigidBody()->setAngularFactor(btVector3(0.0f, 1.0f, 0.0f));
 
 	//set restrictions for head movement
-	user.head->GetRigidBody()->setLinearFactor(btVector3(1.0f, 0.0f, 1.0f));
-	user.head->GetRigidBody()->setAngularFactor(btVector3(0.0f, 1.0f, 0.0f));
-	user.projectile = NULL;
+	//user.head->GetRigidBody()->setLinearFactor(btVector3(1.0f, 0.0f, 1.0f));
+	//user.head->GetRigidBody()->setAngularFactor(btVector3(0.0f, 1.0f, 0.0f));
+	
+
 }
 
 UserTank::~UserTank(){
@@ -32,8 +35,8 @@ void UserTank::Render(GLint modelMatrix, Uniform scalar, Uniform spec, Uniform s
   glUniformMatrix4fv(modelMatrix, 1, GL_FALSE, glm::value_ptr(user.base->GetModel()));
   user.base->Render(scalar, spec, spot, height, eyePos);
 
-  glUniformMatrix4fv(modelMatrix, 1, GL_FALSE, glm::value_ptr(user.head->GetModel()));
-  user.head->Render(scalar, spec, spot, height, eyePos);
+  /*glUniformMatrix4fv(modelMatrix, 1, GL_FALSE, glm::value_ptr(user.head->GetModel()));
+  user.head->Render(scalar, spec, spot, height, eyePos);*/
 
   glUniformMatrix4fv(modelMatrix, 1, GL_FALSE, glm::value_ptr(user.placeholder->GetModel()));
   user.placeholder->Render(scalar, spec, spot, height, eyePos);
@@ -113,6 +116,7 @@ void UserTank::Update(std::vector <unsigned int> keyPress, int mouseMovement,int
 				previousMouse = mouseMovement;
 			break;
 
+	
 		}
 	}
 	// if (mouseMovement != 0){
@@ -120,14 +124,14 @@ void UserTank::Update(std::vector <unsigned int> keyPress, int mouseMovement,int
 	// 	user.head->rotate (glm::vec3(0.0f, 360.0/800 * (mouseMovement - previousMouse), 0.0f));
 	// }
 
-	user.base->GetRigidBody()->getMotionState()->getWorldTransform(lower);
+	/*user.base->GetRigidBody()->getMotionState()->getWorldTransform(lower);
 	upper = lower;
 	upperPos = upper.getOrigin();
 	upperPos.setY(upperPos.getY()+6.6); 
 	upper.setOrigin(upperPos);
-	user.head->GetRigidBody()->proceedToTransform(upper);
-	glm::vec4 headPosition = user.head->getPosition();
-	user.placeholder->translateModel(headPosition);
+	//user.head->GetRigidBody()->proceedToTransform(upper);*/
+	glm::vec4 basePosition = user.base->getPosition();
+	user.placeholder->translateModel(basePosition);
 
 	SetOrientation(); 
 }
@@ -167,9 +171,9 @@ Object* UserTank::GetBase(){
 	return user.base;
 }
 
-Object* UserTank::GetHead(){
+/*Object* UserTank::GetHead(){
 	return user.head;
-}
+}*/
 
 Object* UserTank::GetPlaceholder(){
 	return user.placeholder;
@@ -177,8 +181,13 @@ Object* UserTank::GetPlaceholder(){
 
 void UserTank::SetOrientation(){
 	user.base->setOrientation();
-	user.head->setOrientation();
+	//user.head->setOrientation();
 	if (user.projectile != NULL){
 		user.projectile->setOrientation();
 	}
+}
+
+glm::vec4 UserTank::getPosition()
+{
+	return user.base->getPosition();
 }
